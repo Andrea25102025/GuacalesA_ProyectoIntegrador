c1d4ead460336055b4c589d657b3242c63450e29
Andrea Guacales <andreguacales1@gmail.com>
2026-07-13 23:09:20 -0500

    Reinicio del proyecto con estructura Jakarta EE

--- GuacalesA_ProyectoIntegrador/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GuacalesA_ProyectoIntegrador.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
